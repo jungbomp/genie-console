eae0d3b71e413b6bc9f93dfcfc11582387ea5d2a
Jungbom Pak <jungbomp@gmail.com>
2023-09-30 00:53:13 -0700

    Support Auto complete search (#12)

--- webpack/index.js
+++ webpack/index.js
@@ -38,6 +38,10 @@ module.exports = (envObj, args) => {
         ...ALIAS.PUBLIC,
         ...ALIAS.IMAGES,
       },
+      fallback: {
+        stream: false,
+        buffer: false,
+      },
     },
     plugins: [
       PLUGINS.DOTENV(env),
