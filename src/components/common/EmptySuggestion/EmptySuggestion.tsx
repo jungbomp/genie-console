ebd6262c98c8cfba0341f2aea61e08c84449ef95
RyanKhPark <53807537+RyanKhPark@users.noreply.github.com>
2023-10-15 10:47:48 +0900

    new font applied

--- src/components/common/EmptySuggestion/EmptySuggestion.tsx
+++ src/components/common/EmptySuggestion/EmptySuggestion.tsx
@@ -4,19 +4,16 @@ import Classnames from 'classnames';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-import genieTvInitialSymbolWhite from 'images/Genie_TV_Initial_Symbol_White.png';
+import genieTvInitialSymbolWhite2 from 'images/Genie_TV_Initial_Symbol_White2.png';
 
 import type { EmptySuggestionProps } from './EmptySuggestion.types';
 
 import styles from './EmptySuggestion.scss';
 
-const EmptySuggestion: React.FC<EmptySuggestionProps> = ({ className, title, subtitle }) => (
+const EmptySuggestion: React.FC<EmptySuggestionProps> = ({ className, subtitle }) => (
   <Box className={Classnames(styles.emptySuggestion, className)}>
     <Box className={styles.titleWrapper}>
-      <Box className={styles.logoWrapper}>
-        <img className={styles.logo} src={genieTvInitialSymbolWhite} alt='GENIE' />
-      </Box>
-      <Typography className={styles.title}>{title}</Typography>
+      <img className={styles.logo} src={genieTvInitialSymbolWhite2} alt='GENIE' />
     </Box>
     <Box className={styles.subtitleWrapper}>
       <Typography className={styles.subtitle}>{subtitle}</Typography>
